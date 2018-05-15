ef67f5aa681d200322a5822ab6982b15516f47a7
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-05-15 11:36:50 +0900

    Update, input pattern change.

--- rplugin/python3/deoplete/sources/neko.py
+++ rplugin/python3/deoplete/sources/neko.py
@@ -5,11 +5,11 @@ class Source(Base):
     def __init__(self, vim):
         super().__init__(vim)
         self.name = 'Bignyanco'
-        self.mark = '[neko_dict]'
+        self.mark = '[neko_dictionary]'
         self.input_pattern = (r'^ruby\.')
 
     def get_complete_position(self, context):
-        m = re.search(r'\w*$', context['input'])
+        m = re.search(r'[^. *\t]\w*$', context['input'])
         return m.start() if m else -1
 
     def gather_candidates(self, context):

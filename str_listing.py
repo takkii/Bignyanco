75024ecfd81a877ec675e0aa63c0cc30a3406cd6
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-05-19 19:28:30 +0900

    Update, user test.

---
+++ str_listing.py
@@ -0,0 +1,8 @@
+import re
+import os.path
+
+fi = open(os.path.expanduser('~/.config/nvim/repos/github.com/takkii/Bignyanco/complete/ruby_complete'),'r')
+line = fi.readline()
+fi.close()
+
+print line

2ab269d318ac454c09dff9207f24372e4918cf1a
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-05-19 16:35:32 +0900

    Update, user test.

--- rplugin/python3/deoplete/sources/neko.py
+++ rplugin/python3/deoplete/sources/neko.py
@@ -16,7 +16,6 @@ class Source(Base):
 
     def gather_candidates(self, context):
         fi = open(os.path.expanduser('~/.config/nvim/repos/github.com/takkii/Bignyanco/complete/ruby_complete'),'r')
-        line = fi.read()
+        line = fi.readline()
         fi.close()
-
         return line

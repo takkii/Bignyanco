3160d7e725e694e923bc23ecb7a49c7237e3f378
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-05-23 00:21:41 +0900

    Update.

--- rplugin/python3/deoplete/sources/neko.py
+++ rplugin/python3/deoplete/sources/neko.py
@@ -17,7 +17,7 @@ class Source(Base):
     def bubble_sort(arr):
         change = True
         while change:
-        change = False
+            change = False
         for i in range(len(arr) - 1):
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]

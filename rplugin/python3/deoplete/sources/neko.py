35609f67b0bb931b48916080ecb0a054223a274f
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-01-27 17:47:50 +0900

    Update.

--- rplugin/python3/deoplete/sources/neko.py
+++ rplugin/python3/deoplete/sources/neko.py
@@ -291,7 +291,7 @@ class Source(Base):
         m = re.search('[a-zA-Z0-9_?!]*$', context['input'])
         return m.start() if m else -1
 
-    def gather_candidates(self):
+    def gather_candidates(self, context):
         try:
             dic = ruby + denki + sql + gc + mini_test + mini_test_reporter + unit_test
             dic.sort(key=lambda dic: dic[0])
